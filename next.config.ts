06c4b0abaf3e6a6986a80da64cb5af5a9f0e2389
najib <narujib13@gmail.com>
2026-09-15 16:42:09 +0700

    chore(config): remove env injection in favor of cloudflare runtime secrets

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  env: {
-    TURNSTILE_SECRET_KEY: process.env.TURNSTILE_SECRET_KEY,
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-    CONTACT_EMAIL_TO: process.env.CONTACT_EMAIL_TO,
-    CONTACT_EMAIL_FROM: process.env.CONTACT_EMAIL_FROM,
-  },
   images: {
     remotePatterns: [
       {
